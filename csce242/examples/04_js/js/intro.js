9ab80555a9b9aec6ea4680a780fa76c5ba3c0d7b
Whiteig-CIS <whiteig@email.sc.edu>
2025-09-11 22:33:02 -0400

    project 3 started. Header added to most pages

--- csce242/examples/04_js/js/intro.js
+++ csce242/examples/04_js/js/intro.js
@@ -24,8 +24,43 @@ document.getElementById("btn-click-me").onclick = sayHello;
 
         ///// Iterate on this for efficiency ^^ ////
 
-document.getElementById("btn-click-me").onclick = () => {
-    console.log("hello world");
+document.getElementById("btn-click-me").onclick = (event) => {
+    document.getElementById("p-welcome").innerHTML = "Hello World";  // document.getElementById("btn-click-me").classList.add("clicked");
+
+    event.currentTarget.classList.add("clicked"); // <-- element that was clicked
+};
+
+
+
+const happy = document.getElementById("btn-happy");
+const sad = document.getElementById("btn-sad");
+const clear = document.getElementById("btn-clear");
+const par = document.getElementById("p-happy");
+
+
+happy.onclick = (event) => {
+    par.innerHTML = "Im Happy";  // document.getElementById("btn-click-me").classList.add("clicked");
+    sad.className = ""; // this can lead to issues. use --> sad.classList.remove();
+    event.currentTarget.classList.add("happy"); // <-- element that was clicked
+};
+
+sad.onclick = (event) => {
+    par.innerHTML = "Im Sad";  // document.getElementById("btn-click-me").classList.add("clicked");
+    happy.className = "";
+    sad.classList.add("sad"); // <-- element that was clicked
+};
+
+clear.onclick = (event) => {
+    par.innerHTML = "";  // document.getElementById("btn-click-me").classList.add("clicked");
+    happy.className = "";
+    sad.className = "";
+    
+};
+
+document.getElementById("txt-emotion").onkeyup = (event) => {
+    const userInput = event.currentTarget.value;
+    document.getElementById("p-emotion").innerHTML = `You are feeling ${userInput}.`;
+    document.getElementById("img-emotion").classList.remove("hidden");
 };
 
 
