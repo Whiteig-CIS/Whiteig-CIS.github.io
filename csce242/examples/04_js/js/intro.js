db210d5043b560ed5b7460d87698af92dc101206
Whiteig-CIS <whiteig@email.sc.edu>
2025-09-18 20:33:07 -0400

    home-auto look good on large resolutions

--- csce242/examples/04_js/js/intro.js
+++ csce242/examples/04_js/js/intro.js
@@ -57,10 +57,117 @@ clear.onclick = (event) => {
     
 };
 
-document.getElementById("txt-emotion").onkeyup = (event) => {
+document.getElementById("text-emotion").onkeyup = (event) => {
     const userInput = event.currentTarget.value;
     document.getElementById("p-emotion").innerHTML = `You are feeling ${userInput}.`;
     document.getElementById("img-emotion").classList.remove("hidden");
 };
+
+
+
+document.getElementById("color-show").onclick = (event) => {
+    const colorInput = document.getElementById("mood-color").value.trim().toLowerCase(); // trim whitespace and lowecase
+    const display = document.getElementById("display-color");
+    display.innerHTML = ""; // clear out the <p> display
+    const error = document.getElementById("error-color").innerHTML = ""; // reset error
+    let mood = ""; // 'let' because we are modifying this
+    
+    if (colorInput == "") {
+        error.innerHTML = "* blank"; 
+        return;
+    }
+
+switch (colorInput) {
+    case "red":
+        display.style.color = "red";
+        mood = "angry";
+        break;
+    case "blue":
+        display.style.color = "blue";
+        mood = "sad";
+        break;
+    case "yellow":
+        display.style.color = "yellow" ;
+        mood = "mellow";
+        break;
+  // ... more cases
+  default:
+        error.innerHTML = "*invalid color";
+        return;
+    break;
+}
+
+display.innerHTML = `You chose ${colorInput}, so you are feeling ${mood}`;
+}
+
+
+
+/* Counter */
+let counter = 0;
+let counterInterval;
+const countP = document.getElementById("p-count");
+
+const startBtn = document.getElementById("btn-count-start");
+const pauseBtn = document.getElementById("btn-count-pause");
+const resetBtn = document.getElementById("btn-count-reset");
+
+pauseBtn.disabled = true;
+
+
+document.getElementById("btn-count-start").onclick = () => {
+    startBtn.disabled = true;
+    pauseBtn.disabled = false;
+    counterInterval = setInterval(()=>{
+        countP.innerHTML = counter;
+        counter++;
+    }, 1000);
+   
+}
+
+document.getElementById("btn-count-pause").onclick = () => {
+    startBtn.disabled = false;
+    pauseBtn.disabled = true;
+    clearInterval(counterInterval);
+}
+
+document.getElementById("btn-count-reset").onclick = () => {
+    clearInterval(counterInterval);
+    counter = 0;
+    countP.innerHTML = counter;
+}
+
+/* donations */
+
+const goal = 10000;
+
+document.getElementById("goal-span").innerHTML = goal;
+
+const donateBtn = document.getElementById("btn-donations");
+
+donateBtn.onclick = () => {
+    const donation = document.getElementById("txt-donation").value;
+    const errorSpan = document.getElementById("donation-error");
+    errorSpan.innerHTML = "";
+    console.log(donation);
+
+    if(isNaN(donation) || donation <= 0) {
+        errorSpan.innerHTML = "* Invalid Amount";
+        return;
+    }
+
+    const donationPercent = donation / goal * 100;
+    const message = document.getElementById("goal-message");
+    console.log(donationPercent);
+
+    
+        if (donationPercent < 25) {
+            console.log("x");
+            message.innerHTML = "lets get started";
+        }
+        
+        document.querySelector(":root").style.setProperty("--donation-percent", donationPercent + "%");
+        
+       
+}
 
 
