49af73f1596aede20b82ce481bf11f47bc85ace2
Whiteig-CIS <whiteig@email.sc.edu>
2025-09-25 22:25:25 -0400

    assignment 6 completed

---
+++ csce242/assignments/06loops/loops.js
@@ -0,0 +1,54 @@
+const drawBtn = document.getElementById("draw");
+const clouds = document.getElementById("clouds");
+const trees = document.getElementById("trees");
+const moon = document.getElementById("moon");
+const sun = document.getElementById("sun");
+
+
+
+
+
+
+
+drawBtn.onclick = () => {
+
+    clouds.innerHTML = trees.innerHTML = moon.innerHTML = sun.innerHTML = ""; // clears both clouds and trees
+    trees.classList.toggle("hide");
+    
+
+
+
+    const date = new Date();
+    const hour = date.getHours();
+
+    if (hour >= 18 || hour <= 6) {
+        document.getElementById("body").classList.add("night");
+        const newMoon = document.createElement("div");
+        newMoon.classList.add("moon");
+        moon.append(newMoon);
+    }
+    else {
+        const newSun = document.createElement("div");
+        newSun.classList.add("sun");
+        sun.append(newSun);
+    }
+    
+
+    /* i coded all the logic but used ai to help with the delay feature bc i thought it was cool */
+    /*Loop through and add trees and clouds*/
+    function draw(i) {
+        if (i >= 6) return;
+        const cloud = document.createElement("div");
+        cloud.classList.add("cloud");
+        const tree = document.createElement("div");
+        tree.classList.add("tree");
+
+        clouds.append(cloud);
+        trees.append(tree);
+
+        setTimeout(() => draw(i + 1), 1000); // delay next iteration
+    }
+    draw(0);
+}
+
+
--- csce242/examples/04_js/js/loops.js
+++ csce242/examples/04_js/js/loops.js
@@ -30,4 +30,57 @@ document.getElementById("btn-count").onclick = () => {
         return;
     }
 
+    document.createElement("ul");
+    for (let i=0; i <= end; ++i) {
+        const p = document.createElement("p");
+        p.innerHTML = i;
+        resultDiv.append(p);
+        p.onclick = () => {console.log(`you clicked # ${i}`)};
+    }
+};
+
+document.getElementById("btn-show").onclick = () => {
+    const toyList = document.getElementById("toy-list");
+    toyList.innerHTML = "";
+
+    const toys = ["ball", "skipping rope", "doll", "mini car", "elmo"];
+
+    /* first method to iterate 
+    for (let i = 0; i <= toys.length; ++i) {
+        const toy = document.createElement("li");
+        toy.innerHTML = toys[i];
+        toyList.append(toy);
+    } */
+
+       /* second method */
+       toys.forEach((toy, i) => {
+        const li = document.createElement("li");
+        li.innerHTML = toy;
+        toyList.append(li);
+       });
+}
+
+document.getElementById("btn-show-price").onclick = () => {
+    const toys = [];
+    toys["ball"] = 2.99;
+    toys["barbie"] = 19.99;
+    toys["skipping rope"] = 1.98;
+    toys["mini car"] = 3.88;
+
+    const toyDiv = document.getElementById("toys");
+    toyDiv.innerHTML = "";
+
+    for (let toy in toys) {
+        const p = document.createElement("p");
+        p.innerHTML = toy;
+        toyDiv.append(p);
+        const prices = document.getElementById("prices");
+
+        p.onclick = () => {
+            prices.innerHTML = "";
+            const price = document.createElement("p");
+            price.innerHTML = toy + " is $"+toys[toy];
+            prices.append(price);
+        };
+    }
 };
