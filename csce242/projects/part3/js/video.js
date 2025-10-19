e87aa69e4dc593a5c75ed8712ea880b8d20b72b7
Whiteig-CIS <whiteig@email.sc.edu>
2025-10-18 20:07:35 -0400

    assignment 7

---
+++ csce242/projects/part3/js/video.js
@@ -0,0 +1,5 @@
+let tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+let firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
