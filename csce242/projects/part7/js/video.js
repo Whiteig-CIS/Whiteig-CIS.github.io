bb433b799f7dbf53b15f807110e01885721c5fbb
Whiteig-CIS <whiteig@email.sc.edu>
2025-11-02 23:53:14 -0500

    react site added to homepage and style of homepage changed

--- csce242/projects/part7/js/video.js
+++
@@ -1,5 +0,0 @@
-let tag = document.createElement('script');
-
-tag.src = "https://www.youtube.com/iframe_api";
-let firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
